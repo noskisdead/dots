30cc9ea1412fcd5175fee49d1b09f254dce5a99f
noskisdead <noskisdead@proton.me>
2024-12-01 15:49:37 -0500

    updated zen config

--- config/home/.zen/noskisdead.Default/prefs.js
+++ config/home/.zen/noskisdead.Default/prefs.js
@@ -1,3 +1,4 @@
+user_pref("app.normandy.first_run", false);
 user_pref("browser.startup.page", 1);
 user_pref("startup.homepage_welcome_url.additional", "");
 user_pref("zen.welcomeScreen.seen", true);

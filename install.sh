38775507878804924e8b73929b47bb1d42183849
noskisdead <noskisdead@proton.me>
2025-01-01 17:29:23 -0500

    changed battery notification

--- install.sh
+++ install.sh
@@ -20,11 +20,12 @@ function info_message() {
 # Packages to be installed
 pkglist=(
     wget ttf-jetbrains-mono-nerd ripgrep signal-desktop-beta
-    fisher eza udiskie hyprlock-git grub-btrfs npm ruby thunar pavucontrol-qt qt6ct
-    xdg-desktop-portal-hyprland-git pokemon-colorscripts-git openssh kvantum-git vesktop-git
+    fisher eza udiskie hyprlock-git grub-btrfs npm ruby thunar pavucontrol-qt
+    xdg-desktop-portal-gtk xdg-desktop-portal-hyprland-git cbatticon
+    openssh kvantum-git vesktop-git ark bat pokemon-colorscripts-git
     noto-fonts-emoji hypridle-git pamixer otf-font-awesome zen-browser-bin
     swaync waybar-git fish hyprshot fzf noto-fonts-cjk sddm-astronaut-theme
-    starship wl-clipboard python-pip yazi-git neovim-git zen-browser-bin bat ark
+    starship wl-clipboard python-pip yazi-git neovim-git zen-browser-bin
     mpv feh polkit-gnome sddm-git kitty-git rofi-wayland swww zellij
     zoxide hyprland-git qt5-wayland brightnessctl ttf-cascadia-code-nerd
 )

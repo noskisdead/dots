f765500124927bbf5dbfaba6a1b3a033166a1212
noskisdead <noskisdead@proton.me>
2025-01-06 13:09:35 -0500

    idk fr

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ pkglist=(
     wget ttf-jetbrains-mono-nerd signal-desktop thunar-archive-plugin
     fisher eza udiskie hyprlock grub-btrfs thunar pavucontrol-qt
     xdg-desktop-portal-gtk xdg-desktop-portal-hyprland pokemon-colorscripts-git
-    kvantum vesktop unzip p7zip unrar ark bat uwsm
+    kvantum vesktop unzip p7zip unrar ark bat uwsm obsidian
     noto-fonts-emoji hypridle pamixer otf-font-awesome zen-browser-bin
     swaync waybar fish hyprshot fzf noto-fonts-cjk sddm-astronaut-theme
     starship wl-clipboard yazi neovim zen-browser-bin

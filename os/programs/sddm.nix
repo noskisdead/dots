6ebfc11c2ca3f86591ed60041559a0c8b7bdd6a9
noskisdead <noskisdead@users.noreply.github.com>
2026-01-16 15:24:01 -0500

    doas

--- os/programs/sddm.nix
+++ os/programs/sddm.nix
@@ -1,35 +1,11 @@
 {
-  pkgs,
-  lib,
-  ...
-}: let
-  sddmTheme = pkgs.sddm-astronaut.override {
-    embeddedTheme = "jake_the_dog";
+  services.displayManager = {
+    autoLogin = {
+      enable = true;
+      user = "kenny";
+    };
+
+    defaultSession = "hyprland";
+    ly.enable = true;
   };
-in {
-  services.displayManager.defaultSession = "hyprland";
-
-  services.displayManager.autoLogin = {
-    enable = true;
-    user = "kenny";
-  };
-
-  services.displayManager.sddm = {
-    enable = true;
-    wayland.enable = true;
-    package = lib.mkForce pkgs.kdePackages.sddm;
-    theme = "sddm-astronaut-theme";
-
-    # Dependencies go here so SDDM can load them
-    extraPackages = with pkgs; [
-      kdePackages.qtsvg
-      kdePackages.qtmultimedia
-      kdePackages.qtvirtualkeyboard
-    ];
-  };
-
-  # Theme goes here so it links to /run/current-system/sw/share/sddm/themes
-  environment.systemPackages = [
-    sddmTheme
-  ];
 }

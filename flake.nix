b60b7e8b49bef9e92cf5eaf5b38a86f4884df8fe
noskisdead <noskisdead@users.noreply.github.com>
2026-06-22 17:27:14 -0400

    switching to noctalia next commit

--- flake.nix
+++ flake.nix
@@ -2,15 +2,16 @@
   description = "nosk's everyday dots :3";
 
   inputs = {
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-26.05";
     caelestia-shell.url = "github:caelestia-dots/shell";
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
     #nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    zen-browser.url = "github:youwen5/zen-browser-flake";
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
     nvf.url = "github:notashelf/nvf";
 
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     caelestia-shell.inputs.nixpkgs.follows = "nixpkgs";
     nvf.inputs.nixpkgs.follows = "nixpkgs";
--- todo.md
+++ todo.md
@@ -1,4 +1,4 @@
-better importing of shit
+better importing of shit make a common folder for a lotta shit
 
 impermanence secrets management with sops-nix switch to zsh
 
